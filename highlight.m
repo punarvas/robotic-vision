64cdd3bf46d1280fbfad39bf0eed86f2c7beec5b
Akash Mahajan <akashmahajan025@gmail.com>
2024-01-16 17:19:05 -0500

    Add highlighter

---
+++ highlight.m
@@ -0,0 +1,15 @@
+% Highlight important pixels
+function highlightedImage = highlight(image, intensity)
+    [height, width, ~] = size(image);
+    highlightedImage = image;
+    genm = mean(mean(mean(image)));
+    for i = 1:width
+        for j = 1:height
+            m = mean(image(i, j, :));
+            if (genm - intensity <= m) && (m <= genm + intensity)
+                highlightedImage(i, j, :) = [255, 255, 0];
+            end
+        end
+    end
+
+end
