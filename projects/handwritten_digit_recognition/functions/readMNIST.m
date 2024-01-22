176a99a991f03278fb809976b65787e043271b52
Akash Mahajan <akashmahajan025@gmail.com>
2024-01-21 19:38:06 -0500

    Add SVM

--- projects/handwritten_digit_recognition/functions/readMNIST.m
+++ projects/handwritten_digit_recognition/functions/readMNIST.m
@@ -1,19 +1,30 @@
-function images = readMNIST(image_file_path)
+function [nSamples, images, labels] = readMNIST(image_file_path, labels_file_path)
     % Read file
-    file = fopen(image_file_path, "rb");
+    im_file = fopen(image_file_path, "rb");
+    lb_file = fopen(labels_file_path, "rb");
+
 
     % read meta information on the file
-    magicNumber = fread(file, 1, "int32", 0, "ieee-be");
-    nImages = fread(file, 1, "int32", 0, "ieee-be");
-    nRows = fread(file, 1, 'int32', 0, "ieee-be");
-    nCols = fread(file, 1, "int32", 0, "ieee-be");
+    im_magicNumber = fread(im_file, 1, "int32", 0, "ieee-be");
+    lb_magicNumber = fread(lb_file, 1, "int32", 0, "ieee-be");
 
-    % Read images
-    images = fread(file, inf, "uint8");
-    fclose(file);
+    % image file meta information
+    nImages = fread(im_file, 1, "int32", 0, "ieee-be");
+    nRows = fread(im_file, 1, 'int32', 0, "ieee-be");
+    nCols = fread(im_file, 1, "int32", 0, "ieee-be");
+
+    % labels file meta information
+    nSamples = fread(lb_file, 1, "int32", 0, "ieee-be");
+
+    % Read images and labels
+    images = fread(im_file, inf, "uint8");
+    labels = fread(lb_file, inf, "uint8");
+    fclose(im_file);
+    fclose(lb_file);
 
     % Reshape and permute images
     images = reshape(images, nCols, nRows, nImages);
     images = permute(images, [2, 1, 3]);
+    labels = permute(labels, [2, 1]);
 end
 
--- projects/handwritten_digit_recognition/recognition.m
+++ projects/handwritten_digit_recognition/recognition.m
@@ -4,7 +4,23 @@ clear;
 addpath("functions")
 
 % Read images using readMNIST function
-images = readMNIST("MNIST//train//train-images.idx3-ubyte");
+image_file_path = "MNIST//train//train-images.idx3-ubyte";
+labels_file_path = "MNIST//train//train-labels.idx1-ubyte";
+[nSamples, images, labels] = readMNIST(image_file_path, labels_file_path);
 
-% print size of images
-disp(size(images))
+% data pre-processing
+X = reshape(images, [], size(images, 3))';
+X = double(X) / 255;
+Y = labels;
+
+% Split data into training and testing dataset
+trainSize = 0.8;
+range = round(trainSize * nSamples);
+
+X_train = X(1:range, :);
+X_test = X(range+1:end, :);
+Y_train = Y(1:range);
+Y_test = Y(range+1:end);
+
+% Fit multi-class SVM model
+model = fitcecoc(X_train, Y_train, "Coding", "onevsall", "Verbose", true);
