66f586567fedc3f9f4df438d66211d45654f14c1
Akash Mahajan <akashmahajan025@gmail.com>
2024-01-21 21:48:43 -0500

    Add train and test

--- projects/handwritten_digit_recognition/test.m
+++ projects/handwritten_digit_recognition/test.m
@@ -8,6 +8,31 @@ image_file_path = "MNIST//test//t10k-images.idx3-ubyte";
 labels_file_path = "MNIST//test//t10k-labels.idx1-ubyte";
 [nSamples, images, labels] = readMNIST(image_file_path, labels_file_path);
 
-% Load model
-model = load("model.mat");
-predictions = predict(model, )
+% data pre-processing
+X = reshape(images, [], size(images, 3))';
+X = double(X) / 255;
+Y = labels;
+
+% Split data into training and testing dataset
+trainSize = 0.8;
+range = round(trainSize * nSamples);
+X_test = X(range+1:end, :);
+Y_test = Y(range+1:end);
+
+% load model
+load("model.mat");
+
+% predict
+predictions = predict(model, X_test)';
+
+% Evaluate the model
+accuracy = sum(predictions == Y_test) / numel(Y_test);
+disp(["System accuracy" num2str(accuracy * 100)]);
+
+% Confusion matrix
+% Create confusion matrix
+C = confusionmat(Y_test, predictions);
+
+% Display confusion matrix
+disp('Confusion Matrix:');
+disp(C);
--- projects/handwritten_digit_recognition/train.m
+++ projects/handwritten_digit_recognition/train.m
@@ -18,30 +18,10 @@ trainSize = 0.8;
 range = round(trainSize * nSamples);
 
 X_train = X(1:range, :);
-X_test = X(range+1:end, :);
 Y_train = Y(1:range);
+X_test = X(range+1:end, :);
 Y_test = Y(range+1:end);
 
 % Fit multi-class SVM model
-% model = fitcecoc(X_train, Y_train, "Coding", "onevsall", "Verbose", true);
+model = fitcecoc(X_train, Y_train, "Coding", "onevsall", "Verbose", true);
 
-% load model
-load("model.mat");
-
-% predict
-predictions = predict(model, X_test)';
-
-% Evaluate the model
-% disp(size(Y_test));
-% disp(size(predictions));
-accuracy = sum(predictions == Y_test) / numel(Y_test);
-disp(["System accuracy" num2str(accuracy * 100)]);
-
-
-% Confusion matrix
-% Create confusion matrix
-C = confusionmat(Y_test, predictions);
-
-% Display confusion matrix
-disp('Confusion Matrix:');
-disp(C);
