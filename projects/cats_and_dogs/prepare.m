a4345acd6616bc9ee2ac0cc42bba3c35e367068f
Akash Mahajan <akashmahajan025@gmail.com>
2024-01-29 21:09:34 -0500

    Add prepare dataset

---
+++ projects/cats_and_dogs/prepare.m
@@ -0,0 +1,33 @@
+% clear screen and memory
+clear;
+clc;
+
+% Read images
+dirlist = ["PetImages\Cat\*.jpg", "PetImages\Dog\*.jpg"];
+pathlist = ["PetImages\Cat\", "PetImages\Dog\"];
+
+images = {};
+labels = {};
+
+for c = 1:numel(dirlist)
+    files = dir(dirlist(c));
+    for i = 1:numel(files)
+        filename = fullfile(pathlist(c), files(i).name);
+        try
+            image = imread(filename);
+            image = rgb2gray(image);
+        catch
+            continue;
+        end
+        % Resize and convert image to grayscale
+        image = imresize(image, [224, 224]);
+
+        % save image and corresponding label
+        images = [images, image];
+        labels = [labels, c-1];
+        disp(["Processed ", filename]);
+    end
+end
+
+disp("Statistics on image read");
+disp(["Total images:", num2str(numel(images))]);
