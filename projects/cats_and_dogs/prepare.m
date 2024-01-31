16d43663b4f5adf44a3d5d1e01273d246603c081
Akash Mahajan <akashmahajan025@gmail.com>
2024-01-30 20:22:49 -0500

    Minor update

--- projects/cats_and_dogs/prepare.m
+++ projects/cats_and_dogs/prepare.m
@@ -2,37 +2,26 @@
 clear;
 clc;
 
-% Read images
-dirlist = ["PetImages\Cat\*.jpg", "PetImages\Dog\*.jpg"];
-pathlist = ["PetImages\Cat\", "PetImages\Dog\"];
+% program parameters
+parentfolder = "PetImages";
+classes = ["Cat", "Dog"];
+fileformat = "*.jpg";
+testsize = 0.2;  
+valsize = 0.3;  % percentage of number of images used for test and validation
 
-images = {};
-labels = {};
-
-for c = 1:numel(dirlist)
-    files = dir(dirlist(c));
-    for i = 1:numel(files)
-        filename = fullfile(pathlist(c), files(i).name);
+% Read all class images
+for c = 1:numel(classes)
+    files = dir(fullfile(parentfolder, classes(c), fileformat));
+    for n = 1:numel(files)
+        imagepath = fullfile("PetImages", "Cat", files(i).name);
         try
-            image = imread(filename);
-            image = rgb2gray(image);
+            image = imread(imagepath);
         catch
-            continue;
+            continue
         end
+        image = im2gray(image);
         % Resize and convert image to grayscale
         image = imresize(image, [224, 224]);
-
-        % save image and corresponding label
-        images = {images, image};
-        labels = {labels, c-1};
-        disp(["Processed", filename]);
+        image = reshape(image, 1, []);
     end
 end
-
-disp("Statistics on image read");
-disp(["Total images:", num2str(numel(images))]);
-
-% Save dataset
-savepath = "Dataset.mat";
-save(savepath, "images", "labels");
-disp("Images and corresponding labels stored in the Dataset.mat");
